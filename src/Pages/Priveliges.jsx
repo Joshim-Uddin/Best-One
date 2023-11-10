0541c2dca7f5cbcfd27e7a24a2949c95167fa5e5
Joshim-Uddin <mju881@gmail.com>
2023-11-10 22:11:33 +0600

    previleges finalize

--- src/Pages/Priveliges.jsx
+++ src/Pages/Priveliges.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { FaTruck } from 'react-icons/fa';
+import { FaHandHoldingUsd, FaTruck } from 'react-icons/fa';
+import {LiaCertificateSolid} from 'react-icons/lia'
+import {TbTruckReturn} from 'react-icons/tb'
 
 const Priveliges = () => {
     return (
-        <div className='bg-[#111111] flex flex-col md:flex-row md:gap-8  gap-3 rounded-sm py-8 justify-center items-center'>
+        <div className='bg-[#111111] flex flex-col md:flex-row md:gap-8  gap-3 rounded-sm py-8 justify-center items-center my-8'>
             <div className='flex justify-center items-center gap-4 bg-[#303030] text-white p-8 rounded-sm'>
                 <FaTruck className='text-3xl text-lime-600'></FaTruck>
                 <div>
@@ -12,24 +14,24 @@ const Priveliges = () => {
                 </div>
             </div>
             <div className='flex justify-center items-center gap-4 bg-[#303030] text-white p-8 rounded-sm'>
-                <FaTruck className='text-3xl'></FaTruck>
+                <LiaCertificateSolid className='text-3xl text-lime-600'></LiaCertificateSolid>
                 <div>
-                <h2 className='text-2xl font-bold'>Quick Delivery</h2>
-                <h4 className='font-semibold'>Safety ensured</h4>
+                <h2 className='text-2xl font-bold'>Certifed Quality</h2>
+                <h4 className='font-semibold'>Authority tested</h4>
                 </div>
             </div>
             <div className='flex justify-center items-center gap-4 bg-[#303030] text-white p-8 rounded-sm'>
-                <FaTruck className='text-3xl'></FaTruck>
+                <FaHandHoldingUsd className='text-3xl text-lime-600'></FaHandHoldingUsd>
                 <div>
-                <h2 className='text-2xl font-bold'>Quick Delivery</h2>
-                <h4 className='font-semibold'>Safety ensured</h4>
+                <h2 className='text-2xl font-bold'>Saving Assured</h2>
+                <h4 className='font-semibold'>At lower cost</h4>
                 </div>
             </div>
             <div className='flex justify-center items-center gap-4 bg-[#303030] text-white p-8 rounded-sm'>
-                <FaTruck className='text-3xl'></FaTruck>
+                <TbTruckReturn className='text-3xl text-lime-600'></TbTruckReturn>
                 <div>
-                <h2 className='text-2xl font-bold'>Quick Delivery</h2>
-                <h4 className='font-semibold'>Safety ensured</h4>
+                <h2 className='text-2xl font-bold'>Standard Return Policy</h2>
+                <h4 className='font-semibold'>Easy to return</h4>
                 </div>
             </div>
         </div>
