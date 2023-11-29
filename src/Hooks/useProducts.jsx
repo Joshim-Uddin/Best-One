d0b9f55efbe400897b8bf0ac184ef1c5f69052a9
Joshim-Uddin <mju881@gmail.com>
2023-11-29 07:05:13 +0600

    random product display done

--- src/Hooks/useProducts.jsx
+++ src/Hooks/useProducts.jsx
@@ -2,8 +2,17 @@ import React, { useEffect, useState } from 'react';
 
 const useProducts = () => {
     const [products, setProducts] = useState([])
+    
     useEffect(() =>{
-        fetch('/products.json').then(res=>res.json()).then((data)=>setProducts(data));
+        fetch('/products.json').then(res=>res.json()).then((data)=>{
+            //To create a random number of index to shuffle the products randomly
+            let randomProducts = []
+            for(let j=0; j<data.length-1; j++){
+                randomProducts.push(data[(Math.floor(Math.random() * data.length))])
+            }
+            
+            setProducts(randomProducts)
+        });
     }, [])
     return products;
 };
