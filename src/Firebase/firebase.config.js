350bd0e48962db794bb90a0e1279171534ca49af
Joshim-Uddin <mju881@gmail.com>
2024-06-25 22:47:41 +0600

    authentication modified

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID
+  // apiKey: import.meta.env.VITE_APIKEY,
+  // authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  // projectId: import.meta.env.VITE_PROJECTID,
+  // storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  // messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  // appId: import.meta.env.VITE_APPID
+  apiKey: "AIzaSyAgnPLoGPNePz0bJ2c7iwAy9abl-R_b9f8",
+  authDomain: "best-one-36ee0.firebaseapp.com",
+  projectId: "best-one-36ee0",
+  storageBucket: "best-one-36ee0.appspot.com",
+  messagingSenderId: "953692256859",
+  appId: "1:953692256859:web:50c3b3baae7feb7bd71d7e"
 };
 
 // Initialize Firebase
